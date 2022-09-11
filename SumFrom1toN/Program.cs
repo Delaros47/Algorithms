b482ea96a8ac1965ec72be6a616e6c21a814856b
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-09-11 12:24:41 +0300

    ...

---
+++ SumFrom1toN/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumFrom1toN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1+2+3+4+5+...+250=n sum
+
+            Console.Write("Write a number to be summed from 1 until our number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Total : " +SumOfNNumber(number));
+
+
+            Console.ReadLine();
+        }
+
+        private static int SumOfNNumber(int n)
+        {
+
+            var total = 0;
+            for (int i = 1; i <= n; i++)
+            {
+
+                total = total + i;
+
+            }
+
+            return total;
+        }
+    }
+}
