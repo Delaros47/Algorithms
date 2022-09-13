51904091ea7e0cd4e0572e41b1653bea100d6a86
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-09-13 11:34:51 +0300

    ...

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            for (int i = 1; i <=10 ; i++)
+            {
+                Console.ForegroundColor = (ConsoleColor)new Random().Next(1, 16);
+                Console.WriteLine($"*******{i}*******");
+                for (int j = 1; j <=10; j++)
+                {
+
+                    Console.WriteLine($"{i}x{j} = {i*j}");
+
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number for calculation Prime numbers : ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            PrimeNumbers(number);
+
+            Console.ReadLine();
+        }
+
+        private static void PrimeNumbers(int number)
+        {
+            int totalPrimeNumber=0;
+            for (int i = 2; i <=number; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <=i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        counter++;
+                    }
+                    if(counter>1) 
+                        break;
+
+                }
+
+                if (counter > 1)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is a Prime Number ");
+                    totalPrimeNumber++;
+                }
+                counter=0;
+
+            }
+
+            Console.WriteLine("Total Prime numbers = "+totalPrimeNumber);
+
+        }
+    }
+}
