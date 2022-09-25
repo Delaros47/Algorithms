a6b9b7ced8c4f109f516df1a95ed6331866fe656
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-09-25 09:58:48 +0300

    ..

---
+++ BinaryToDecimal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BinaryToDecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string binary = "10000101";
+            double total = 0;
+
+            for (int i = binary.Length-1,x=0; i >=0; i--)
+            {
+                total = total + (Convert.ToInt32(binary[i].ToString()) * Math.Pow(2, x));
+                x++;
+            }
+
+            Console.WriteLine($"{binary} = {total}");
+
+            Console.ReadLine();
+        }
+    }
+}
